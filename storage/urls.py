adb1e8f85c236e4b7d15c570a85e1b4cb0d88613
Dsh <knowslana@gmail.com>
2023-12-08 18:19:51 +0300

    login on main page

--- storage/urls.py
+++ storage/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path, include
 from django.conf.urls.static import static
 
@@ -22,6 +23,7 @@ from extended_user.views import ProfileDetailView, SignUp
 from storage import settings
 
 urlpatterns = [
+    path('', LoginView.as_view()),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('users/', include('extended_user.urls')),
