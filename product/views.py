3927bc334f155a4288b495bb4fff51fd959ea101
Fisunov <fisunoff@yandex.ru>
2023-12-09 04:43:59 +0300

    диаграмма в карточке product

--- product/views.py
+++ product/views.py
@@ -1,17 +1,18 @@
+import json
+from random import randint
+
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db import models
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
 from django_tables2 import SingleTableView
 
 from mixins import AddTitleFormMixin, ProDetailView, SaveEditorMixin, DetailWithSingleTable
-from operation.filters import OperationFilter
+from operation.const import ADMISSION, DEPARTURE, RECALC
 from operation.models import Operation
-from operation.tables import SmallOperationTable, OperationByProductTable
+from operation.tables import  OperationByProductTable
 from product.models import Product
 from product.tables import ProductTable
-from stock.models import Stock
-from stock.tables import StockTable
+
 
 
 class ProductCreateView(LoginRequiredMixin, SaveEditorMixin, AddTitleFormMixin, CreateView):
@@ -41,6 +42,36 @@ class ProductDetailView(DetailWithSingleTable):
         if self.request.user.is_authenticated:
             can_edit = self.request.user.profile.pk == self.object.creator or self.request.user.is_superuser
         kwargs['can_edit'] = can_edit
+        stocks = {}
+        operations = Operation.objects.filter(product=self.object)
+        for elem in operations:
+            if elem.type in (ADMISSION, DEPARTURE, RECALC):
+                if elem.stock.name not in stocks:
+                    stocks[elem.stock.name] = 0
+                if elem.type in (ADMISSION, RECALC):
+                    stocks[elem.stock.name] += elem.quantity
+                else:
+                    stocks[elem.stock.name] += elem.quantity
+            else:
+                if elem.from_stock.name not in stocks:
+                    stocks[elem.from_stock.name] = 0
+                if elem.to_stock.name not in stocks:
+                    stocks[elem.to_stock.name] = 0
+                stocks[elem.from_stock.name] -= elem.quantity
+                stocks[elem.to_stock.name] += elem.quantity
+
+        diagram_labels = []
+        diagram_data = []
+        for stock in stocks:
+            if stocks[stock] > 0:
+                diagram_labels.append(stock)
+                diagram_data.append(stocks[stock])
+
+        kwargs['diagram_labels'] = json.dumps(diagram_labels)
+        kwargs['diagram_data'] = diagram_data
+        kwargs['colors'] = [
+            f'rgba({randint(50, 200)}, {randint(50, 200)}, {randint(50, 200)}, 0.7)' for i in range(len(diagram_data))
+        ]
         return kwargs
 
     table_model = Operation
